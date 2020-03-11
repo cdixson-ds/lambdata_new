d227e64f1107735b9608b83d4bd574fdecff1955
cdixson <crystal.dixson@gmail.com>
2020-03-11 12:22:31 -0700

    Setup repo using conda for virtual environment

---
+++ knockClass.py
@@ -0,0 +1,30 @@
+#knock.py
+
+#class Joke(self):
+    #def_init_(self):
+        #pass
+
+#def Greeting(self):
+#    print(f"Knock Knock!")
+    #input()
+
+#def Who(self):
+#    return f"{self['answer1']}{self['answer2']self['answer3']}"
+
+
+if __name__=="__main__":
+    jokes = [
+        {"joke":"A little old lady","punch": "I didn't know you could yodel."},
+        {"joke":"Mustache", "punch": "I mustache you a question, but I'll shave it for later"},
+        {"joke":"Tank", "punch": "You're welcome"},
+    ]
+
+    for joke in jokes:
+        print("Knock Knock")
+        input()
+        print(joke)
+        input()
+        print(punch)
+        
+
+
---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="cdixsonlambdata", 
+    version="1.0",
+    author="C Dixson",
+    author_email="crystal.dixson@gmail.com",
+    description="A small example package",
+    #long_description=long_description,
+    #long_description_content_type="check for nulls, split a dataframe, tell me a knock knock joke",
+    url="https://github.com/cdixson-ds/lambdata-12",
+    packages=setuptools.find_packages()
+)
